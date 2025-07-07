369d8d60df30c7ce753e7710cd82679a89059456
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-07-07 14:57:03 +0200

    add class Biglietto

--- src/main/java/buildWeek/entities/Biglietto.java
+++ src/main/java/buildWeek/entities/Biglietto.java
@@ -1,4 +1,92 @@
 package buildWeek.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "biglietti")
 public class Biglietto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private LocalDate dataemissione;
+    @ManyToOne
+    @JoinColumn(name = "distrubutori_id", nullable = false, unique = true)
+    private Distributore distributore;
+    private boolean validazione=false;
+    private LocalDate orariovalidazione;
+    @ManyToOne
+    @JoinColumn(name = "mezzo_id", nullable = false, unique = true)
+    private Mezzo idmezzo;
+
+    public Biglietto(){}
+
+    public Biglietto( LocalDate dataemissione, Distributore distributore, boolean validazione, LocalDate orariovalidazione, Mezzo idmezzo) {
+
+        this.dataemissione = dataemissione;
+        this.distributore = distributore;
+        this.validazione = validazione;
+        this.orariovalidazione = orariovalidazione;
+        this.idmezzo = idmezzo;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+
+
+    public LocalDate getDataemissione() {
+        return dataemissione;
+    }
+
+    public void setDataemissione(LocalDate dataemissione) {
+        this.dataemissione = dataemissione;
+    }
+
+    public Distributore getDistributore() {
+        return distributore;
+    }
+
+    public void setDistributore(Distributore distributore) {
+        this.distributore = distributore;
+    }
+
+    public boolean isValidazione() {
+        return validazione;
+    }
+
+    public void setValidazione(boolean validazione) {
+        this.validazione = validazione;
+    }
+
+    public LocalDate getOrariovalidazione() {
+        return orariovalidazione;
+    }
+
+    public void setOrariovalidazione(LocalDate orariovalidazione) {
+        this.orariovalidazione = orariovalidazione;
+    }
+
+    public Mezzo getIdmezzo() {
+        return idmezzo;
+    }
+
+    public void setIdmezzo(Mezzo idmezzo) {
+        this.idmezzo = idmezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "Biglietto{" +
+                "id=" + id +
+                ", dataemissione=" + dataemissione +
+                ", distributore=" + distributore +
+                ", validazione=" + validazione +
+                ", orariovalidazione=" + orariovalidazione +
+                ", idmezzo=" + idmezzo +
+                '}';
+    }
 }
