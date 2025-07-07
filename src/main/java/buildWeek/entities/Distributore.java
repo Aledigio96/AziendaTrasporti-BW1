5b33ffa36e8270ea1af4b7d67c67e5f436128916
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-07 12:40:35 +0200

    Creazione della classe Distributore e delle classi figlie

--- src/main/java/buildWeek/entities/Distributore.java
+++ src/main/java/buildWeek/entities/Distributore.java
@@ -1,4 +1,36 @@
 package buildWeek.entities;
 
-public class Distributore {
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "distributore")
+public abstract class Distributore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "zona_partenza", referencedColumnName = "zonaPartenza", nullable = false)
+    private Tratta posizione;
+
+    public Distributore() {
+    }
+
+    public Distributore(Tratta posizione) {
+        this.posizione = posizione;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Tratta getPosizione() {
+        return posizione;
+    }
+
+    public void setPosizione(Tratta posizione) {
+        this.posizione = posizione;
+    }
 }
