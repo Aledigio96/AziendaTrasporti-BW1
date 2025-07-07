6297b9bfae367f5ea0bb48bc71179f0b0b605b5f
Ren-Mor <renatomorelli@hotmail.it>
2025-07-07 11:45:13 +0200

    First commit

--- src/main/java/buildWeek/entities/Tessera.java
+++ src/main/java/buildWeek/entities/Tessera.java
@@ -1,4 +1,91 @@
 package buildWeek.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tessere")
 public class Tessera {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "data_emissione")
+    private LocalDate dataEmissione;
+
+    @Column(name = "data_scadenza")
+    private LocalDate dataScadenza;
+
+    private boolean attiva;
+
+    @OneToOne
+    @JoinColumn(name = "utente_id", nullable = false, unique = true)
+    private Utente utente;
+
+    public Tessera() {}
+
+    public Tessera(LocalDate dataEmissione, LocalDate dataScadenza, boolean attiva, Utente utente) {
+        this.dataEmissione = dataEmissione;
+        this.dataScadenza = dataScadenza;
+        this.attiva = attiva;
+        this.utente = utente;
+    }
+
+    // Getter
+    public UUID getId() {
+        return id;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public LocalDate getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public boolean isAttiva() {
+        return attiva;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    // Setter
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public void setDataScadenza(LocalDate dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+
+    public void setAttiva(boolean attiva) {
+        this.attiva = attiva;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    @Override
+    public String toString() {
+        return "Tessera{" +
+                "id=" + id +
+                ", dataEmissione=" + dataEmissione +
+                ", dataScadenza=" + dataScadenza +
+                ", attiva=" + attiva +
+                ", utente=" + utente +
+                '}';
+    }
 }
--- src/main/java/buildWeek/entities/Utente.java
+++ src/main/java/buildWeek/entities/Utente.java
@@ -1,4 +1,87 @@
 package buildWeek.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "utenti")
 public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    private String nome;
+    private String cognome;
+
+    @Column(name = "data_nascita")
+    private LocalDate dataNascita;
+
+    @OneToOne(mappedBy = "utente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Tessera tessera;
+
+    public Utente() {}
+
+    public Utente(String nome, String cognome, LocalDate dataNascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+    }
+
+    // Getter
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public Tessera getTessera() {
+        return tessera;
+    }
+
+    // Setter
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public void setTessera(Tessera tessera) {
+        this.tessera = tessera;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", dataNascita=" + dataNascita +
+                ", tessera=" + tessera +
+                '}';
+    }
 }
