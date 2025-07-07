b9b9a398ee76d2c3a4baa99a22ab9698532471c9
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-07-07 15:05:08 +0200

    update amministratore class

--- src/main/java/buildWeek/entities/Amministratore.java
+++ src/main/java/buildWeek/entities/Amministratore.java
@@ -1,4 +1,65 @@
 package buildWeek.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "amministratori")
 public class Amministratore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    private String cognome;
+    private String email;
+
+    public Amministratore(){}
+
+    public Amministratore( String nome, String cognome, String email) {
+
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Amministratore{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
