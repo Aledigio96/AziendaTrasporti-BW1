c31bb0a1bc844dfe9bdd37f4ae1614a0300fc391
Ren-Mor <renatomorelli@hotmail.it>
2025-07-08 16:19:29 +0200

    PercorrenzaDao

--- src/main/java/buildWeek/dao/PercorrenzaDAO.java
+++ src/main/java/buildWeek/dao/PercorrenzaDAO.java
@@ -1,4 +1,23 @@
 package buildWeek.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import java.util.UUID;
+
 public class PercorrenzaDAO {
+    private final EntityManager entityManager;
+
+    public PercorrenzaDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public long contaPercorrenzeMezzoTratta(String mezzoId, String trattaId) {
+        Query query = entityManager.createQuery(
+                "SELECT COUNT(p) FROM Percorrenza p WHERE p.idMezzo.id = :mezzoId AND p.idTratta.id = :trattaId"
+        );
+        query.setParameter("mezzoId", mezzoId);
+        query.setParameter("trattaId", trattaId);
+        return (long) query.getSingleResult();
+    }
 }
+
